e2a9e0f362ecae5854763e5911839ea75c3dc869
Andres Lopez <141793922+AndresLopezzz@users.noreply.github.com>
2025-11-17 18:24:56 -0500

    Import App.css instead of app.css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./app.css";
+import "./App.css";
 import { routeTree } from "./routeTree.gen";
 import { ThemeProvider } from "./components/theme";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
